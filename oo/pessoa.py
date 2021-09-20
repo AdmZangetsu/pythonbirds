76c2c0a69b7f4da8bc02a325447265555516b1da
Zangetsu <admzangetsu@Hotmail.com>
2021-09-19 22:33:29 -0300

    Criado atributo completo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=0):
+    def __init__(self, *filhos, nome=None, idade=75):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Julio')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print ( p.nome )
-    p.nome = 'Jefferson'
-    p.idade = '39'
-    print(p.nome)
-    print(p.idade)
+    jefferson = Pessoa (nome='Jefferson' )
+    janezy = Pessoa(jefferson, nome='Janezy')
+
+    print(Pessoa.cumprimentar(janezy))
+    print(id(janezy))
+    print(janezy.cumprimentar())
+    print(janezy.nome)
+    print(janezy.idade)
+    for filho in janezy.filhos:
+        print(filho.nome)
+
